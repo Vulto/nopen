4913d587640775fdb74a524ea4c1771bf65778ff
vulto <jhsoperacao@gmail.com>
2024-01-31 19:50:15 -0300

    Organizing the spaghetti in functions

--- nopen.c
+++ nopen.c
@@ -8,10 +8,53 @@
 
 #define BUFFER_SIZE 1024
 
-void Run(const char *filename, const char *application) {
+void runApplication(const char *filename, const char *application) {
     execlp(application, application, filename, (char *)NULL);
-    perror("execlp");
+    fprintf(stderr, "Could not find application %s", application);
     exit(EXIT_FAILURE);
+}
+
+const char *readFilenameFromStdin() {
+    char buffer[BUFFER_SIZE];
+    ssize_t bytesRead = read(STDIN_FILENO, buffer, sizeof(buffer));
+
+    if (bytesRead <= 0) {
+        fprintf(stderr, "Error reading from stdin\n");
+        exit(EXIT_FAILURE);
+    }
+
+    buffer[bytesRead] = '\0';
+
+    if (bytesRead > 0 && buffer[bytesRead - 1] == '\n') {
+        buffer[bytesRead - 1] = '\0';
+    }
+
+    return strdup(buffer);
+}
+
+const char *getMimeType(const char *filename, magic_t magic) {
+    const char *mime_type = magic_file(magic, filename);
+    if (mime_type == NULL) {
+        fprintf(stderr, "Cannot determine file type: %s\n", magic_error(magic));
+        magic_close(magic);
+        exit(EXIT_FAILURE);
+    }
+    return mime_type;
+}
+
+const char *findApplicationForFileType(const char *mime_type) {
+    const char *application = NULL;
+    for (size_t i = 0; i < sizeof(fileTypes) / sizeof(FileTypeMapping); ++i) {
+        if (strcmp(fileTypes[i].file_type, mime_type) == 0) {
+            application = fileTypes[i].application;
+            break;
+        }
+    }
+    return application;
+}
+
+void handleNoPredefinedApplication(const char *mime_type) {
+    fprintf(stderr, "No predefined application for file type %s\n", mime_type);
 }
 
 int main(int argc, char *argv[]) {
@@ -20,63 +63,31 @@ int main(int argc, char *argv[]) {
     if (argc == 2) {
         filename = argv[1];
     } else if (argc == 1) {
-        char buffer[BUFFER_SIZE];
-        ssize_t bytesRead = read(STDIN_FILENO, buffer, sizeof(buffer));
-
-        if (bytesRead <= 0) {
-            fprintf(stderr, "Error reading from stdin\n");
-            exit(EXIT_FAILURE);
-        }
-
-        // Null-terminate the string
-        buffer[bytesRead] = '\0';
-
-        // Remove trailing newline character
-        if (bytesRead > 0 && buffer[bytesRead - 1] == '\n') {
-            buffer[bytesRead - 1] = '\0';
-        }
-
-        filename = buffer;
+        filename = readFilenameFromStdin();
     } else {
         fprintf(stderr, "Usage: %s [filename]\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    // Create magic library handle
     magic_t magic = magic_open(MAGIC_MIME_TYPE);
     if (magic == NULL) {
         fprintf(stderr, "Unable to initialize magic library\n");
         exit(EXIT_FAILURE);
     }
 
-    // Load the default magic database
     if (magic_load(magic, NULL) != 0) {
         fprintf(stderr, "Cannot load magic database: %s\n", magic_error(magic));
         magic_close(magic);
         exit(EXIT_FAILURE);
     }
 
-    // Get the MIME type of the file
-    const char *mime_type = magic_file(magic, filename);
-    if (mime_type == NULL) {
-        fprintf(stderr, "Cannot determine file type: %s\n", magic_error(magic));
-        magic_close(magic);
-        exit(EXIT_FAILURE);
-    }
-
-    // Find the associated application in the mapping
-    const char *application = NULL;
-    for (size_t i = 0; i < sizeof(fileTypes) / sizeof(FileTypeMapping); ++i) {
-        if (strcmp(fileTypes[i].file_type, mime_type) == 0) {
-            application = fileTypes[i].application;
-            break;
-        }
-    }
+    const char *mime_type = getMimeType(filename, magic);
+    const char *application = findApplicationForFileType(mime_type);
 
     if (application != NULL) {
-        Run(filename, application);
+        runApplication(filename, application);
     } else {
-        fprintf(stderr, "No predefined application for file type %s\n", mime_type);
+        handleNoPredefinedApplication(mime_type);
     }
 
     magic_close(magic);
